4eb0bdd18bff9bb70d0e59e5444c66c09b8eb590
Avinash-5559 <sinha.avinash906@gmail.com>
2025-05-08 20:07:43 +0530

    Commit

--- src/Java_Basic_Programs/Date.java
+++ src/Java_Basic_Programs/Date.java
@@ -5,10 +5,9 @@ public class Date {
         byte dt = 8;
         char mth = 'M';
         int yr = 2025;
-        System.out.println("\n");
+        System.out.println();
         System.out.println("Date : "+dt);
         System.out.println("Month : "+mth);
         System.out.println("Year : "+yr);
-        System.out.println();
     }
 }
