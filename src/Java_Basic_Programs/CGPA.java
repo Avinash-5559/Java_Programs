1b32310c312f69aa3b6ff7bad3018f127ecf7660
Avinash-5559 <sinha.avinash906@gmail.com>
2025-05-08 20:54:43 +0530

    Commit

--- src/Java_Basic_Programs/CGPA.java
+++ src/Java_Basic_Programs/CGPA.java
@@ -1,4 +1,15 @@
 package Java_Basic_Programs;
 
 public class CGPA {
+    public static void main(String [] args) {
+        float Num1=79.3f;
+        float Num2=65.6F;
+        float Num3=42.7f;
+        float cp = (Num1+Num2+Num3)/30;
+        System.out.println("Output : "+cp);
+    }
 }
+
+/*
+Output : 6.253333
+*/
