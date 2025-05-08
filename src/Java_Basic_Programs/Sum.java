39541f4f6656c876576b903a21cac8ec4cfc9b3a
Avinash-5559 <sinha.avinash906@gmail.com>
2025-05-08 20:47:56 +0530

    Commit

--- src/Java_Basic_Programs/Sum.java
+++ src/Java_Basic_Programs/Sum.java
@@ -1,4 +1,13 @@
 package Java_Basic_Programs;
 
 public class Sum {
+    public static void main(String [] args) {
+        int x, y, z;
+        x=y=z=50;
+        System.out.println(x+(y-1)+(z+4));
+    }
 }
+
+/*
+Output : 153
+*/
