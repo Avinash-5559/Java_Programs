385d7bac86461701fd590447535eca829bdeffad
Avinash-5559 <sinha.avinash906@gmail.com>
2025-05-08 18:59:33 +0530

    Commit

--- src/Java_Basic_Programs/DataTypes.java
+++ src/Java_Basic_Programs/DataTypes.java
@@ -1,4 +1,23 @@
 package Java_Basic_Programs;
 
 public class DataTypes {
+    public static void main(String [] args) {
+        byte month = 5;
+        short year = 2025;
+        int pcode = 201309;
+        long phone = 7899565265L;
+        float perc = 82.35F;
+        double tax = 14.26568565478;
+        char grade = 'A';
+        boolean pass = true;
+        System.out.println();
+        System.out.println("Current Month : "+month);
+        System.out.println("Current Year : "+year);
+        System.out.println("Area Pincode : "+pcode);
+        System.out.println("Phone Number : "+phone);
+        System.out.println("Percentage :"+perc);
+        System.out.println("Tax Value : "+tax);
+        System.out.println("Grade :"+grade);
+        System.out.println("Pass :"+pass);
+    }
 }
