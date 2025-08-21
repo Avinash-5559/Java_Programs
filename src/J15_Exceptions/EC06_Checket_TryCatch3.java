f25f6f76c8fe4cefeac7c81a1176211f89b01245
HP <sinha.avinash906@gmail.com>
2025-08-21 21:39:24 +0530

    Commit

---
+++ src/J15_Exceptions/EC06_Checket_TryCatch3.java
@@ -0,0 +1,30 @@
+package J15_Exceptions;
+
+public class EC06_Checket_TryCatch3 {
+    public static void main(String[] args) {
+
+        int a = 0;
+        int b = 0;
+        try {
+            b = 10 / a;
+            String name = null;
+            name.trim();
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+        catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+
+        /*
+        catch (ArithmeticException | NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+        */
+
+        System.out.println(b);
+    }
+}
