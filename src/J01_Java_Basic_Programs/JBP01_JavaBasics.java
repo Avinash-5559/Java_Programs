98e07f93448fc740b1ac07d88a18ead4cd456447
HP <sinha.avinash906@gmail.com>
2025-08-26 15:05:52 +0530

    Commit

---
+++ src/J01_Java_Basic_Programs/JBP01_JavaBasics.java
@@ -0,0 +1,36 @@
+package J01_Java_Basic_Programs;
+
+public class JBP01_JavaBasics {
+    public static void main(String[] args) {
+
+        /*
+
+        package                 → Groups related classes together in a folder-like structure.
+                                  i.e. "Folder name for the class."
+
+        J01_Java_Basic_Programs → Name of the Package.
+
+        public                  → Access modifier that makes a class or method visible to all other classes.
+                                  i.e. “Visible to everyone, even JVM.”
+
+        class                   → Blueprint to create objects; it defines variables and methods.
+                                  i.e. “Blueprint box for code.”
+
+        JBP01_JavaBasics        → Name of the Class.
+
+        static                  → Belongs to the class, not to an object; can be called without creating an object.
+                                  i.e. “Runs without object.”
+
+        void                    → Return type that means the method does not give back any value.
+                                  i.e. “Returns nothing.”
+
+        main                    → Special method where the Java program starts execution.
+                                  i.e. “Program’s starting door.”
+
+        String\[] args          → Array of strings that stores command-line arguments passed to the program.
+                                  i.e. “Carries inputs from command line.”
+
+        */
+
+    }
+}
