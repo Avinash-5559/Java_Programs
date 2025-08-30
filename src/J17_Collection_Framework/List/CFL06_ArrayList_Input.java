56f25b95b0a898a7ba8b5f92eb0f8f36a8f36936
HP <sinha.avinash906@gmail.com>
2025-08-30 13:33:34 +0530

    Commit

--- src/J17_Collection_Framework/List/CFL06_ArrayList_Input.java
+++ src/J17_Collection_Framework/List/CFL06_ArrayList_Input.java
@@ -1,7 +1,48 @@
 package J17_Collection_Framework.List;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CFL06_ArrayList_Input {
     public static void main(String[] args) {
-//LAB 234
+
+        // Multiple inputs from the user and store them in separate.
+        // ArrayLists based on user-defined categories.
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> names = new ArrayList<>();
+
+        ArrayList<Integer> ages = new ArrayList<>();
+
+        String continueInput = "Y";                                     // Control variable for input loop
+
+        while (continueInput.equalsIgnoreCase("Y")) {
+
+            System.out.print("\nEnter the Name : ");
+            String name = sc.next();
+
+            names.add(name);                                            // Add name to names list
+
+            System.out.print("\nEnter the Age : ");
+            int age = sc.nextInt();
+
+            ages.add(age);                                              // Add age to ages list
+
+            System.out.print("\nDo you want to Enter Another Record ? (Y/N) : ");
+            continueInput = sc.next();
+
+        }
+
+        for (Object o1 : names) {
+            System.out.println(o1);
+        }
+
+        for (Object o2 : ages) {
+            System.out.println(o2);
+        }
+
+        sc.close();
+
     }
 }
