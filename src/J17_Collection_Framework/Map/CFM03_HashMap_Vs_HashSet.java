3769edaf07152f7160b5a5c274da64881f150430
HP <sinha.avinash906@gmail.com>
2025-09-03 13:12:17 +0530

    Commit

---
+++ src/J17_Collection_Framework/Map/CFM03_HashMap_Vs_HashSet.java
@@ -0,0 +1,50 @@
+package J17_Collection_Framework.Map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class CFM03_HashMap_Vs_HashSet {
+    public static void main(String[] args) {
+
+        System.out.println("\n--------------------------------- HashMap ---------------------------------\n");
+
+        Map<String, Object> student1 = new HashMap();
+
+        student1.put("name", "Dipankar");
+        student1.put("phone", "9876543210");
+        student1.put("address", "Bangalore");
+        student1.put("address", 123);
+
+        System.out.println("HashMap Items : " + student1);
+
+        /*
+            OUTPUT :
+                    --------------------------------- HashMap ---------------------------------
+
+                    HashMap Items : {address=123, phone=9876543210, name=Dipankar}
+        */
+
+        System.out.println("\n--------------------------------- HashSet ---------------------------------\n");
+
+        Set book_read_items = new HashSet();
+
+        book_read_items.add("Rich dad Poor Dad");
+        book_read_items.add("Sapiens");
+        book_read_items.add("Secret");
+        book_read_items.add("Atomic Habit");
+        book_read_items.add("Atomic Habit");
+        book_read_items.add("Eat the Frog");
+
+        System.out.println("HashSet Items : " + book_read_items);
+
+        /*
+            OUTPUT :
+                    --------------------------------- HashSet ---------------------------------
+
+                    HashSet Items : [Secret, Sapiens, Atomic Habit, Eat the Frog, Rich dad Poor Dad]
+        */
+
+    }
+}
