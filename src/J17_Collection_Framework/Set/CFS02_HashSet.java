07ebff22966aa8991db91c91031bf1fdf918098a
HP <sinha.avinash906@gmail.com>
2025-09-05 12:53:00 +0530

    Commit

---
+++ src/J17_Collection_Framework/Set/CFS02_HashSet.java
@@ -0,0 +1,20 @@
+package J17_Collection_Framework.Set;
+
+import java.util.HashSet;
+
+public class CFS02_HashSet {
+    public static void main(String[] args) {
+
+        HashSet set = new HashSet();
+
+        set.add("Orange");
+        set.add("Apple");
+        set.add("Mango");
+        set.add(null);
+        set.add(953656);
+        set.add("WaterMelon");
+        set.add("Apple");
+
+        System.out.println("HashSet List Items : " + set);
+    }
+}
