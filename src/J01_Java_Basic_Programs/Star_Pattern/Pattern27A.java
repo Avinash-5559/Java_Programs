d6e2b17208184a6cbc27f5388be67f6a13f367eb
HP <sinha.avinash906@gmail.com>
2025-09-10 12:17:00 +0530

    Commit

---
+++ src/J01_Java_Basic_Programs/Star_Pattern/Pattern27A.java
@@ -0,0 +1,31 @@
+package J01_Java_Basic_Programs.Star_Pattern;
+
+public class Pattern27A {
+    public static void main(String[] args) {
+
+        int row = 7;
+        int spc = 7;
+        int str = 7;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < spc ; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < str; j++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
+            spc--;
+        }
+    }
+}
+
+/*
+                *  *  *  *  *  *  *
+              *  *  *  *  *  *  *
+            *  *  *  *  *  *  *
+          *  *  *  *  *  *  *
+        *  *  *  *  *  *  *
+      *  *  *  *  *  *  *
+    *  *  *  *  *  *  *
+*/
