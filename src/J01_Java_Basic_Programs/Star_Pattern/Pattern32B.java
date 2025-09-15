1a92a56a3c18502c0080b1daf100be611851e2d0
HP <sinha.avinash906@gmail.com>
2025-09-15 13:51:04 +0530

    Commit

---
+++ src/J01_Java_Basic_Programs/Star_Pattern/Pattern32B.java
@@ -0,0 +1,51 @@
+package J01_Java_Basic_Programs.Star_Pattern;
+
+import java.util.Scanner;
+
+public class Pattern32B {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int r = 1;
+
+        while ((r > 0) && (r <= 1)) {
+
+            System.out.print("\nEnter Number of Rows : ");
+            int row = sc.nextInt();
+
+            System.out.print("\nEnter Number of Symbol : ");
+            int str = sc.nextInt();
+
+            System.out.print("\nEnter the Symbol : ");
+            String sym = sc.next();
+
+            System.out.println();
+
+            for (int i = 1; i <= row; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(sym + " ");
+                }
+                for (int j = 1; j <= 2 * (str - i); j++) {
+                    System.out.print("  ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(sym + " ");
+                }
+                System.out.println();
+            }
+            System.out.print("\nPress 1 to Continue OR Press Any Number to Exit : ");
+            r = sc.nextInt();
+        }
+        if (r != 1) {
+            System.out.println("\nThank You.");
+        }
+    }
+}
+
+/*
+    *                 *
+    * *             * *
+    * * *         * * *
+    * * * *     * * * *
+    * * * * * * * * * *
+*/
