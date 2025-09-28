d902a70066cb37d197df77b02f749c48c3c0b7a3
HP <sinha.avinash906@gmail.com>
2025-09-28 11:14:06 +0530

    Java Programs

--- src/J01_Java_Basic_Programs/Star_Pattern/Pattern27A.java
+++ src/J01_Java_Basic_Programs/Star_Pattern/Pattern27A.java
@@ -3,16 +3,16 @@ package J01_Java_Basic_Programs.Star_Pattern;
 public class Pattern27A {
     public static void main(String[] args) {
 
-        int row = 7;
-        int spc = 7;
-        int str = 7;
+        int row = 12;
+        int spc = 12;
+        int str = 12;
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < spc ; j++) {
                 System.out.print("  ");
             }
             for (int j = 0; j < str; j++) {
-                System.out.print("*  ");
+                System.out.print("* ");
             }
             System.out.println();
             spc--;
@@ -21,11 +21,16 @@ public class Pattern27A {
 }
 
 /*
-                *  *  *  *  *  *  *
-              *  *  *  *  *  *  *
-            *  *  *  *  *  *  *
-          *  *  *  *  *  *  *
-        *  *  *  *  *  *  *
-      *  *  *  *  *  *  *
-    *  *  *  *  *  *  *
+               * * * * * * * * * * * *
+              * * * * * * * * * * * *
+             * * * * * * * * * * * *
+            * * * * * * * * * * * *
+           * * * * * * * * * * * *
+          * * * * * * * * * * * *
+         * * * * * * * * * * * *
+        * * * * * * * * * * * *
+       * * * * * * * * * * * *
+      * * * * * * * * * * * *
+     * * * * * * * * * * * *
+    * * * * * * * * * * * *
 */
