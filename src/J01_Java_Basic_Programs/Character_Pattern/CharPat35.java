a08ae9d0a9914fff568644034db1eeee2a0b6ce1
HP <sinha.avinash906@gmail.com>
2025-09-29 13:40:24 +0530

    Java Programs

---
+++ src/J01_Java_Basic_Programs/Character_Pattern/CharPat35.java
@@ -0,0 +1,36 @@
+package J01_Java_Basic_Programs.Character_Pattern;
+
+public class CharPat35 {
+    public static void main(String[] args) {
+
+        int row = 12;
+        int str = 12;
+        char c = 'Z';
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < str; j++) {
+                if (i == 0 || i == row - 1 || j == row - 1 - i) {
+                    System.out.print(c + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+    Z Z Z Z Z Z Z Z Z Z Z Z
+                        Z
+                      Z
+                    Z
+                  Z
+                Z
+              Z
+            Z
+          Z
+        Z
+      Z
+    Z Z Z Z Z Z Z Z Z Z Z Z
+*/
