de22804508fbd0b93346f5de32b4809c37efbaf2
HP <sinha.avinash906@gmail.com>
2025-10-09 00:05:14 +0530

    Java Programs

---
+++ src/J01_Java_Basic_Programs/Number_Pattern/NumPat27.java
@@ -0,0 +1,33 @@
+package J01_Java_Basic_Programs.Number_Pattern;
+
+public class NumPat27 {
+    public static void main(String[] args) {
+
+        int row = 9;
+        int str = 16;
+        int num = 1;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < str; j++) {
+                if (i == 0 || j == 0 || j == str - 1 || i == row - 1) {
+                    System.out.print(num + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+    1                             1
+    1                             1
+    1                             1
+    1                             1
+    1                             1
+    1                             1
+    1                             1
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+*/
