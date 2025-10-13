833daf4412f6c564996f8ab00f9eee78b2d90f16
HP <sinha.avinash906@gmail.com>
2025-10-13 15:15:40 +0530

    Java Programs

---
+++ src/J01_Java_Basic_Programs/Star_Pattern/Pattern40B.java
@@ -0,0 +1,63 @@
+package J01_Java_Basic_Programs.Star_Pattern;
+
+import java.util.Scanner;
+
+public class Pattern40B {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int r = 1;
+
+        while (r == 1) {
+
+            System.out.print("\nEnter the Number of Rows : ");
+            int row = sc.nextInt();
+
+            System.out.print("\nEnter the Number of Space : ");
+            int spc = sc.nextInt();
+
+            System.out.print("\nEnter the Number of Symbol : ");
+            int str = sc.nextInt();
+
+            System.out.print("\nEnter the Symbol : ");
+            String sym = sc.next();
+
+            System.out.println();
+
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < spc; j++) {
+                    System.out.print("  ");
+                }
+                for (int j = 0; j < str; j++) {
+                    if (i == 0 || j == 0 || i == row - 1 || j == str - 1) {
+                        System.out.print(sym + " ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+                spc++;
+            }
+            System.out.print("\nPress 1 to Continue OR Press Any Number to Exit : ");
+            r = sc.nextInt();
+        }
+        if (r != 1) {
+            System.out.println("\nThank You.");
+        }
+    }
+}
+
+/*
+    * * * * * * * * * * * *
+     *                     *
+      *                     *
+       *                     *
+        *                     *
+         *                     *
+          *                     *
+           *                     *
+            *                     *
+             *                     *
+              *                     *
+               * * * * * * * * * * * *
+*/
