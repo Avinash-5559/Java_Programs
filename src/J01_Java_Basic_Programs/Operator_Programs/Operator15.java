e72deac8f6df7fdb7eeaea0905c6fe453dd1e9ae
Avinash Sinha <sinha.avinash906@gmail.com>
2025-11-19 15:29:28 +0530

    Java Programs

--- src/J01_Java_Basic_Programs/Operator_Programs/Operator15.java
+++ src/J01_Java_Basic_Programs/Operator_Programs/Operator15.java
@@ -1,9 +1,9 @@
 package J01_Java_Basic_Programs.Operator_Programs;
 
 public class Operator15 {
-    public static void main(String [] args) {
-        int i=4;
-        i&=5;
-        System.out.println(i<<1);//8
+    public static void main(String[] args) {
+        int i = 4;
+        i &= 5;
+        System.out.println(i << 1);//8
     }
 }
--- src/J01_Java_Basic_Programs/Operator_Programs/Operator18.java
+++ src/J01_Java_Basic_Programs/Operator_Programs/Operator18.java
@@ -1,9 +1,9 @@
 package J01_Java_Basic_Programs.Operator_Programs;
 
 public class Operator18 {
-    public static void main(String [] args) {
-        int i=5;
-        int j = i++>5?i++:++i;
+    public static void main(String[] args) {
+        int i = 5;
+        int j = i++ > 5 ? i++ : ++i;
         System.out.println(j);//7
         System.out.println(i);//7
     }
