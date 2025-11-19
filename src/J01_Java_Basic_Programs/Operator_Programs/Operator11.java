a757624df3d994ff0d7d66db6c35919edced7ce9
Avinash Sinha <sinha.avinash906@gmail.com>
2025-11-19 15:29:13 +0530

    Java Programs

--- src/J01_Java_Basic_Programs/Operator_Programs/Operator11.java
+++ src/J01_Java_Basic_Programs/Operator_Programs/Operator11.java
@@ -2,10 +2,10 @@ package J01_Java_Basic_Programs.Operator_Programs;
 
 public class Operator11 {
     public static void main(String[] args) {
-        int i=0;
+        int i = 0;
         i++;
         --i;
-        int j=i++ + --i + ++i + i--;
+        int j = i++ + --i + ++i + i--;
         System.out.println(++j);//3
         System.out.println(i--);//0
     }
--- src/J01_Java_Basic_Programs/Operator_Programs/Operator13.java
+++ src/J01_Java_Basic_Programs/Operator_Programs/Operator13.java
@@ -2,11 +2,11 @@ package J01_Java_Basic_Programs.Operator_Programs;
 
 public class Operator13 {
     public static void main(String[] args) {
-        int a=10;
-        int b=10;
-        int c=10;
-        a=a++ + ++b;
-        int d=b++ + ++a + c++;
+        int a = 10;
+        int b = 10;
+        int c = 10;
+        a = a++ + ++b;
+        int d = b++ + ++a + c++;
         System.out.println(d);//43
         System.out.println(c);//11
         System.out.println(b);//12
