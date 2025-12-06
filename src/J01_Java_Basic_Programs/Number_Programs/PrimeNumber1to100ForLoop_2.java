ac884e0c4b99f71f8e50101f587a1e5e236e8534
Avinash Sinha <sinha.avinash906@gmail.com>
2025-12-07 00:02:07 +0530

    Java Programs

--- src/J01_Java_Basic_Programs/Number_Programs/PrimeNumber1to100ForLoop_2.java
+++ src/J01_Java_Basic_Programs/Number_Programs/PrimeNumber1to100ForLoop_2.java
@@ -3,21 +3,21 @@ package J01_Java_Basic_Programs.Number_Programs;
 public class PrimeNumber1to100ForLoop_2 {
     public static void main(String[] args) {
 
-        for (int n=1; n<=100; n++){
+        for (int n = 1; n <= 100; n++) {
 
-            int b=0;
+            int b = 0;
 
-            if(n>=2){
+            if (n >= 2) {
 
-                for (int i = 2; i <=(n-1) ; i++) {
-                    if(n%i==0){
-                        b=b+1;
+                for (int i = 2; i <= (n - 1); i++) {
+                    if (n % i == 0) {
+                        b = b + 1;
                         break;
                     }
                 }
 
-                if (b==0){
-                    System.out.print(n+" ");
+                if (b == 0) {
+                    System.out.print(n + " ");
                 }
             }
         }
