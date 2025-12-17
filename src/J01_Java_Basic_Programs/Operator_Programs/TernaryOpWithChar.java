24755b54dffa2d69ed3bab5b176a6e8b995f9bfa
Avinash Sinha <sinha.avinash906@gmail.com>
2025-12-17 15:56:17 +0530

    Java Programs

--- src/J01_Java_Basic_Programs/Operator_Programs/TernaryOpWithChar.java
+++ src/J01_Java_Basic_Programs/Operator_Programs/TernaryOpWithChar.java
@@ -3,8 +3,8 @@ package J01_Java_Basic_Programs.Operator_Programs;
 public class TernaryOpWithChar {
     public static void main(String[] args) {
         System.out.println();
-        byte i=45;
-        String j= i>=55?"i is Greater than j":"j is Greater than i";
-        System.out.println("Which Number is Greater : "+j);
+        byte i = 45;
+        String j = i >= 55 ? "i is Greater than j" : "j is Greater than i";
+        System.out.println("Which Number is Greater : " + j);
     }
 }
