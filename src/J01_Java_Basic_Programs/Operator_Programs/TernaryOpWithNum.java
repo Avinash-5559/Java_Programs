cb98ea3d8cf9b981f3a862b0d817424c1599f6b1
Avinash Sinha <sinha.avinash906@gmail.com>
2025-12-19 17:54:59 +0530

    Java Programs

--- src/J01_Java_Basic_Programs/Operator_Programs/TernaryOpWithNum.java
+++ src/J01_Java_Basic_Programs/Operator_Programs/TernaryOpWithNum.java
@@ -2,8 +2,8 @@ package J01_Java_Basic_Programs.Operator_Programs;
 
 public class TernaryOpWithNum {
     public static void main(String[] args) {
-        byte i=45;
-        byte j= (byte)(i>=55?45:55);
+        byte i = 45;
+        byte j = (byte) (i >= 55 ? 45 : 55);
         System.out.println(j);//55
     }
 }
