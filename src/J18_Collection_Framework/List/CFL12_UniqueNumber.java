9262bef6a5b6bc8b3de0ece770a14242e4b54760
Avinash Sinha <sinha.avinash906@gmail.com>
2026-04-02 01:36:52 +0530

    Java Programs

---
+++ src/J18_Collection_Framework/List/CFL12_UniqueNumber.java
@@ -0,0 +1,34 @@
+package J18_Collection_Framework.List;
+
+import java.util.ArrayList;
+
+public class CFL12_UniqueNumber {
+    public static void main(String[] args) {
+
+        int[] a = {4, 5, 5, 5, 4, 6, 6, 9, 4};
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < a.length; i++) {
+
+            int k = 0;
+
+            if (!list.contains(a[i])) {
+                list.add(a[i]);
+                k++;
+
+                for (int j = i + 1; j < a.length; j++) {
+                    if (a[j] == a[i]) {
+                        k++;
+                    }
+                }
+
+                if (k == 1) {
+                    System.out.println("\n" + a[i] + " is the Unique Number in the List");
+                }
+
+            }
+        }
+
+    }
+}
