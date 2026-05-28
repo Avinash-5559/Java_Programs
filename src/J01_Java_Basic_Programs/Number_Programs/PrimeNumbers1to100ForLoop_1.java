f67cce65fde80a8eb2c5fda626f3dc2d295e345d
Avinash Sinha <sinha.avinash906@gmail.com>
2026-05-28 13:48:46 +0530

    Java Programs

--- src/J01_Java_Basic_Programs/Number_Programs/PrimeNumbers1to100ForLoop_1.java
+++ src/J01_Java_Basic_Programs/Number_Programs/PrimeNumbers1to100ForLoop_1.java
@@ -19,6 +19,7 @@ public class PrimeNumbers1to100ForLoop_1 {
                     if (prime) {
                         System.out.print(n+ " ");
                 }
+
             }
         }
     }
